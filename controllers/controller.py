83faf6908282c61383cc480f0b666dcec9f4de2f
jersobh <jersobh@gmail.com>
2017-02-18 16:20:01 -0200

    Using models on controllers

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,8 +6,10 @@ controller.py
 from views import render
 from models import db
 from models import gen
+from models import models
 
 def index(request):
     context = {'name': 'Pydge Framework', 'github': 'https://github.com/jersobh/pydge'}
     template = 'index.jinja2'
+    models.Users.create(username='Charlie') #my example has USERS table with username field)
     return render.view(request, template, context)
