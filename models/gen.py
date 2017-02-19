11e237e093f5f09ec255f375e1853a144b013a1a
jersobh <jersobh@gmail.com>
2017-02-19 16:10:24 -0300

    Add host to generate models

--- models/gen.py
+++ models/gen.py
@@ -9,6 +9,7 @@ from core.conf import *
 if GENERATE_MODELS:
     if DB_PASS:
         cmdping = "python -m pwiz -H %s -u %s -P %s -p %s -e %s %s > models/models.py" % (DB_HOST, DB_USER, DB_PASS, DB_PORT, DB_TYPE, DB)
-    else
+    else:
         cmdping = "python -m pwiz -H %s -u %s -p %s -e %s %s > models/models.py" % (DB_HOST, DB_USER, DB_PORT, DB_TYPE, DB)
+        
         p = subprocess.Popen(cmdping, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
